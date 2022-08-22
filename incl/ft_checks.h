15ace6994bdf0d8de4a0e9acbf5f0714be7395b8
Andoni Marzana cuenca <amarzana@c2r6s5.42urduliz.com>
2022-08-22 17:32:02 +0200

    Starting checks

---
+++ incl/ft_checks.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checks.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 17:23:28 by amarzana          #+#    #+#             */
+/*   Updated: 2022/08/22 17:31:00 by amarzana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CHECKS_H
+# define FT_CHECKS_H
+
+# include "ft_philo.h"
+
+void	ft_checks(int argc, char **argv, t_control *control);
+
+#endif
---
+++ incl/ft_philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_philo.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 17:25:43 by amarzana          #+#    #+#             */
+/*   Updated: 2022/08/22 17:29:33 by amarzana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PHILO_H
+# define FT_PHILO_H
+
+# include "ft_checks.h"
+# include "ft_sleep.h"
+
+typedef struct s_control
+{
+	int	error;
+}					t_control;
+
+#endif
